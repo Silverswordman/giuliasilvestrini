3677245f14b62ef5714945b97589b44e148f0573
Giulia Silvestrini <silvergiuly@hotmail.it>
2024-01-24 16:16:16 +0100

    Initial commit

---
+++ src/main/java/S6L3/giuliasilvestrini/S6L3Application.java
@@ -0,0 +1,13 @@
+package S3L1.giuliasilvestrini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiuliasilvestriniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiuliasilvestriniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/S6L3/giuliasilvestrini/S6L3ApplicationTests.java
@@ -0,0 +1,13 @@
+package S3L1.giuliasilvestrini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiuliasilvestriniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
